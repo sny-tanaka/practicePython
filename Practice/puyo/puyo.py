2ada1c4035274ae52cb6c7fef279ab55d3ffb311
sny-tanaka <s05.t12.030@gmail.com>
2018-03-27 11:47:13 +0900

    puyoにスコア表示, 上キーで即落下を追加。
    その他バグ修正およびコードのスマート化。

--- Practice/puyo/puyo.py
+++ Practice/puyo/puyo.py
@@ -67,7 +67,38 @@ def drop_all(puyo_stack):
     return puyo_stack
 
 
-def stack_display(puyo_stack, fp, screen):
+def score_calculation(score, chain, puyo_stack):
+    # 基本点, 消したぷよ×10
+    erase_num = 0
+    for r in range(12):
+        for c in range(6):
+            if puyo_stack[r][c] == 'bomb':
+                erase_num += 1
+    base_score = erase_num * 10
+
+    # 連鎖ボーナス
+    if chain == 1:
+        chain_bonus = 1
+    else:
+        chain_bonus = 8 * (2**(chain-2))
+    score += base_score * chain_bonus
+    return score
+
+
+def make_score_board(score, sysfont, screen):
+    score_board = sysfont.render('{0:06d}'.format(score), True, (255, 0, 0), (255, 255, 0))
+    rect_score_board = score_board.get_rect()
+    rect_score_board.topright = (390, 300)
+    score_bg = sysfont.render('{0:06d}'.format(0), True, (255, 255, 0), (255, 255, 0))
+    rect_score_bg = score_bg.get_rect()
+    rect_score_bg.topright = (390, 300)
+    screen.blit(score_bg, rect_score_bg)
+    screen.blit(score_board, rect_score_board)
+
+
+def stack_display(puyo_stack, fp, screen, on_flame):
+    for part in on_flame:
+        screen.blit(part[0], part[1])
     rect_list = []
     for c in range(6):
         for r in range(12):
@@ -76,8 +107,6 @@ def stack_display(puyo_stack, fp, screen):
             rect_stack = stack.get_rect()
             rect_stack.topleft = (25+c*45, 480-r*40)
             screen.blit(stack, rect_stack)
-            rect_list.append(rect_stack)
-    pygame.display.update(rect_list)
 
 
 def main():
@@ -86,14 +115,15 @@ def main():
     screen = pygame.display.set_mode((400, 565))  # ウィンドウサイズの指定
     pygame.display.set_caption('ぷよぷよ')  # タイトルバー
     bg, rect_bg = load_image('bg', fp)
-    flame, rect_flame = load_image('flame', fp)
-    rect_flame.center = (160, 280)
+    on_flame = []
+    on_flame.append(load_image('flame', fp))
+    on_flame[0][1].center = (160, 280)
     subflame, rect_subflame = load_image('subflame', fp)
     rect_subflame.center = (360, 120)
-    batsu, rect_batsu = load_image('batsu', fp)
-    rect_batsu.topleft = (115, 40)
-    batsu2, rect_batsu2 = load_image('batsu', fp)
-    rect_batsu2.topleft = (160, 40)
+    on_flame.append(load_image('batsu', fp))
+    on_flame[1][1].topleft = (115, 40)
+    on_flame.append(load_image('batsu', fp))
+    on_flame[2][1].topleft = (160, 40)
     push_enter, rect_push_enter = load_image('enter', fp)
     rect_push_enter.center = (200, 280)
     gameover, rect_gameover = load_image('over', fp)
@@ -115,6 +145,10 @@ def main():
     stock2_puyo2_color = puyo2
     rect_stock2_puyo2.center = (360, 185)
 
+    # スコアボード用フォントの作成
+    score = 0
+    sysfont = pygame.font.SysFont(None, 28)
+
     speed = 2
     game_start_flg = False
     game_over_flg = False
@@ -134,7 +168,7 @@ def main():
                 if puyo_stack[r][c] == 'None':
                     ug[c] = 480 - 40*r
                     break
-        
+
         # ゲームオーバー判定
         if game_start_flg:
             if puyo_stack[11][2] != 'None' or puyo_stack[11][3] != 'None':
@@ -142,14 +176,21 @@ def main():
                 game_over_flg = True
 
         screen.blit(bg, rect_bg)
-        screen.blit(flame, rect_flame)
         screen.blit(subflame, rect_subflame)
-        screen.blit(batsu, rect_batsu)
-        screen.blit(batsu2, rect_batsu2)
         screen.blit(stock1_puyo1, rect_stock1_puyo1)
         screen.blit(stock1_puyo2, rect_stock1_puyo2)
         screen.blit(stock2_puyo1, rect_stock2_puyo1)
         screen.blit(stock2_puyo2, rect_stock2_puyo2)
+
+        # スタックぷよの表示
+        if game_start_flg:
+            stack_display(puyo_stack, fp, screen, on_flame)
+        else:
+            for part in on_flame:
+                screen.blit(part[0], part[1])
+
+        # スコアボードの作成
+        make_score_board(score, sysfont, screen)
 
         if game_start_flg:
             if puyo_drop_flg is False:
@@ -184,6 +225,7 @@ def main():
 
                 # 落ちぷよ中の判定へ
                 puyo_drop_flg = True
+                chain = 0
             else:
                 # 落ちぷよを表示
                 rect_drop_puyo1.topleft = (x1, y1)
@@ -192,11 +234,13 @@ def main():
                 screen.blit(drop_puyo2, rect_drop_puyo2)
 
                 # 落ちぷよを下へ移動
-                if y1 != ug[(x1-25)//45] and y2 != ug[(x2-25)//45]:
-                    y1 += speed
-                    y2 += speed
-                else:
-                    puyo_drop_flg = False
+                for i in range(speed):
+                    if y1 != ug[(x1-25)//45] and y2 != ug[(x2-25)//45]:
+                        y1 += 1
+                        y2 += 1
+                    else:
+                        puyo_drop_flg = False
+                        break
         else:
             if game_over_flg:
                 screen.blit(gameover, rect_gameover)
@@ -245,31 +289,23 @@ def main():
             while y1 != to_y1 or y2 != to_y2:
                 pygame.time.Clock().tick(60)
                 if y1 != to_y1:
-                    y1 += speed + 2
+                    y1 += 4
                 if y2 != to_y2:
-                    y2 += speed + 2
+                    y2 += 4
                 rect_drop_puyo1.topleft = (x1, y1)
                 rect_drop_puyo2.topleft = (x2, y2)
-                screen.blit(flame, rect_flame)
-                screen.blit(batsu, rect_batsu)
-                screen.blit(batsu2, rect_batsu2)
-                stack_display(puyo_stack, fp, screen)
+                stack_display(puyo_stack, fp, screen, on_flame)
                 screen.blit(drop_puyo1, rect_drop_puyo1)
                 screen.blit(drop_puyo2, rect_drop_puyo2)
                 pygame.display.update()
             puyo_stack[r1][c1] = drop_puyo1_color
             puyo_stack[r2][c2] = drop_puyo2_color
 
-        # スタックぷよの表示
-        if game_start_flg:
-            stack_display(puyo_stack, fp, screen)
-
         # ぷよが消える判定
         if game_start_flg and puyo_drop_flg is False:
             erase_flg = True
             while erase_flg:
                 erase_flg = False
-                flg = False
                 for r in range(12):
                     for c in range(6):
                         if puyo_stack[r][c] != 'None':
@@ -277,20 +313,30 @@ def main():
                             for i in range(12):
                                 check_flg.append([False, False, False, False, False, False])
                             puyo_stack, erase_flg = erase_puyo(puyo_stack, c, r, 1, check_flg, erase_flg)
-                            if erase_flg:
-                                screen.blit(flame, rect_flame)
-                                screen.blit(batsu, rect_batsu)
-                                screen.blit(batsu2, rect_batsu2)
-                                stack_display(puyo_stack, fp, screen)
-                                puyo_stack = bomb2none(puyo_stack)
-                                puyo_stack = drop_all(puyo_stack)
-                                pygame.time.delay(1000)
-                                stack_display(puyo_stack, fp, screen)
-                                pygame.time.delay(1000)
-                                flg = True
-                                break
-                    if flg:
-                        break
+                if erase_flg:
+                    chain += 1
+                    score = score_calculation(score, chain, puyo_stack)
+                    stack_display(puyo_stack, fp, screen, on_flame)
+                    make_score_board(score, sysfont, screen)
+                    pygame.display.update()
+                    pygame.time.delay(500)
+
+                    puyo_stack = bomb2none(puyo_stack)
+                    stack_display(puyo_stack, fp, screen, on_flame)
+                    pygame.display.update()
+                    pygame.time.delay(500)
+
+                    puyo_stack = drop_all(puyo_stack)
+                    stack_display(puyo_stack, fp, screen, on_flame)
+                    pygame.display.update()
+                    pygame.time.delay(200)
+
+        # 下矢印押しっぱなしでぷよ加速
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[K_DOWN]:
+            if y1 != ug[(x1-25)//45] and y2 != ug[(x2-25)//45]:
+                y1 += 1
+                y2 += 1
 
         pygame.display.update()  # 画面更新
 
@@ -331,6 +377,22 @@ def main():
                         if x1 != 250 and x2 != 250:
                             x1 += 45
                             x2 += 45
+                    if event.key == K_UP:
+                        # ぷよを一瞬で真下に落とす
+                        if y1 != ug[(x1-25)//45] and y2 != ug[(x2-25)//45]:
+                            if x1 != x2:
+                                if ug[(x1-25)//45] < ug[(x2-25)//45]:
+                                    y1 = ug[(x1-25)//45]
+                                    y2 = y1
+                                else:
+                                    y2 = ug[(x2-25)//45]
+                                    y1 = y2
+                            elif y1 > y2:
+                                y1 = ug[(x1-25)//45]
+                                y2 = y1 - 40
+                            else:
+                                y2 = ug[(x2-25)//45]
+                                y1 = y2 - 40
 
 
 if __name__ == '__main__':
